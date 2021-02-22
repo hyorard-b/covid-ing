25b73b690b9aa5e036bd75f70dcdfb4e90fc0117
hyorard-b <reddream08@gmail.com>
2021-02-22 14:30:37 +0900

    [기능] 사이드 네비게이션 올리고 내리기

--- client/src/js/render/side-nav.js
+++ client/src/js/render/side-nav.js
@@ -5,6 +5,16 @@ const renderSideNav = () => {
   sideNavBtn.onclick = () => {
     sideNav.classList.toggle('active');
   };
+
+  sideNav.addEventListener('transitionend', e => {
+    if (!e.target.classList.contains('active')) return;
+    sideNav.style.zIndex = 1;
+  });
+
+  sideNav.addEventListener('transitionstart', e => {
+    if (e.target.classList.contains('active')) return;
+    sideNav.style.zIndex = -99;
+  });
 };
 
 export default renderSideNav;
