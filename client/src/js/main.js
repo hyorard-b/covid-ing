fc785a8d50d5053c72be5ccc9b32c78b3ffb6d22
GEUNA <geuna0204@gmail.com>
2021-02-24 13:58:12 +0900

    [기능구현] 지역별 확진자 데이터 받아오기

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -1,9 +1,13 @@
 import renderSideNav from './render/side-nav';
 import tabHandler from './utils/tabs';
 import barChart from './utils/chart';
+import localInf from './utils/local';
+import renderLocalData from './render/local-infection';
 
 window.addEventListener('DOMContentLoaded', () => {
   renderSideNav();
   tabHandler();
   barChart();
+  localInf();
+  renderLocalData();
 });
