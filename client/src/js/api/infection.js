712fd894036a7b76e874ce9f19a42ed14e432e88
hyorard-b <reddream08@gmail.com>
2021-02-24 20:01:58 +0900

    [기능] 날짜 중복 데이터 제거

--- client/src/js/api/infection.js
+++ client/src/js/api/infection.js
@@ -1,15 +1,26 @@
 import axios from 'axios';
-import { parseISO, sub, format } from 'date-fns';
+import { parseISO, format } from 'date-fns';
 
 const INFECT_URL = 'http://localhost:5000/corona/infect/';
 
 const getInfects = async (startDay, endDay) => {
   // startDay - 1 일 거 부터 가져와야 함
   const startDayObj = parseISO(startDay);
-  const dayBefore = format(sub(startDayObj, { days: 1 }), 'yyyyMMdd');
+  const dayBefore = format(startDayObj, 'yyyyMMdd');
 
   const url = `${INFECT_URL + dayBefore}/${endDay}`;
-  const { data: infects } = await axios.get(url);
+  const { data } = await axios.get(url);
+
+  // 중복 제거
+  const infects = [...data].reduce((infects, infect) => {
+    const isDuplicated =
+      infects.filter(({ stateDt }) => infect.stateDt === stateDt).length > 0;
+
+    if (isDuplicated) return infects;
+    infects.push({ stateDt: infect.stateDt, decideCnt: infect.decideCnt });
+
+    return infects;
+  }, []);
 
   return infects.map(dailyInfect => dailyInfect.decideCnt).reverse();
 };
