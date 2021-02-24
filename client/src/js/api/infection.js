32b70dbc39cf54a75aa78ede853957d4e26ca158
hyorard-b <reddream08@gmail.com>
2021-02-24 11:20:06 +0900

    [기능] 시작 날짜 확진자 계산을 위한 전날 데이터

--- client/src/js/api/infection.js
+++ client/src/js/api/infection.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
+import { parseISO, sub, format } from 'date-fns';
 
 const INFECT_URL = 'http://localhost:5000/corona/infect/';
 
 const getInfects = async (startDay, endDay) => {
-  const url = `${INFECT_URL + startDay}/${endDay}`;
-  const { data } = await axios.get(url);
+  // startDay - 1 일 거 부터 가져와야 함
+  const startDayObj = parseISO(startDay);
+  const dayBefore = format(sub(startDayObj, { days: 1 }), 'yyyyMMdd');
 
-  console.log(data);
+  const url = `${INFECT_URL + dayBefore}/${endDay}`;
+  const { data: infects } = await axios.get(url);
 
-  // return infects.map(({ data }) => data.decideCnt).reverse();
+  return infects.map(dailyInfect => dailyInfect.decideCnt).reverse();
 };
 
 export default getInfects;
