511174f27828066d7aebdd641a2e66bbf598fdf8
hyorard-b <reddream08@gmail.com>
2021-02-25 21:21:10 +0900

    [리팩토링] 사이드 내비게이션 주석 추가

--- client/src/js/render/side-nav.js
+++ client/src/js/render/side-nav.js
@@ -8,15 +8,18 @@ const renderSideNav = () => {
     $sideNav.classList.toggle('active');
   };
 
+  // 사이드 네비게이션 내부 클릭시
   $sideNav.onclick = e => {
     e.stopPropagation();
   };
 
+  // 사이드 내비게이션 외부 클릭시
   document.body.onclick = e => {
     if (e.target === $sideNavBtn) return;
     $sideNav.classList.remove('active');
   };
 
+  // 링크 클릭 시 링크에 해당하는 컨텐츠 컨테이너로 이동
   [...document.querySelector('.nav-list').children].forEach(($link, idx) => {
     $link.onclick = () => {
       $mainContents[idx].scrollIntoView({
